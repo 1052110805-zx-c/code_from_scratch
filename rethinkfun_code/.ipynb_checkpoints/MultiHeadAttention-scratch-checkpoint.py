65541ccefac2d7a51d33627de2c5ca5bafadc56c
1052110805-zx-c <1052110805@qq.com>
2026-03-25 05:31:44 -0400

    code_from_scratch

---
+++ rethinkfun_code/.ipynb_checkpoints/MultiHeadAttention-scratch-checkpoint.py
@@ -0,0 +1,35 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7300e3aa-835c-4307-9f49-d5b312fc3cd9",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.10 (minimind_310)",
+   "language": "python",
+   "name": "minimind_310"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.13"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
